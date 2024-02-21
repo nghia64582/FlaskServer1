ac3afdc5dcc945c128e94e68677074497f6c43c7
nghia64582 <nghia64582@gmail.com>
2024-02-21 20:31:46 +0700

    update handle request

--- app.py
+++ app.py
@@ -16,35 +16,23 @@ def saveDictToJsonFile(data, jsonFile):
 def getDictByJson(jsonFile):
     return json.load(open(jsonFile))
 
-
 @app.route("/", methods = ["GET", "POST"])
 def hello_world():
-    try:
-        if request.content_type == "application/json":
-            print("Request {} with json {}".format(request.method, request.json))
-            if request.json.get("num") != None:
-                num = int(request.json.get("num"))
-                data = {"a": [i for i in range(num)]}
-                saveDictToJsonFile(data, "data1.json")
-                logger.debug("IP {} create json with size {}".format(request.remote_addr, num))
-                return "Create json with size {}".format(num)
-            else:
-                return "Json do not have key <num>"
+    if request.content_type == "application/json" and request.method == "POST":
+        print("Request {} with json {}".format(request.method, request.json))
+        if request.json.get("num") != None:
+            num = int(request.json.get("num"))
+            data = {"a": [i for i in range(num)]}
+            saveDictToJsonFile(data, "data1.json")
+            logger.debug("IP {} create json with size {}".format(request.remote_addr, num))
+            return "Create json with size {}".format(num)
         else:
-            print("Json is required.")
-            return "Json is required."
-
-    except:
-        traceback.print_exc()
-        try:
-            data = getDictByJson("data1.json")
-            logger.debug("Get request from IP {}.".format(request.remote_addr))
-            print("Data {}".format(data))
-            return data
-        except:
-            traceback.print_exc()
-            print("Print time")
-            return "Ver 1.2. Current time is {}".format(time.time())
+            return "Json do not have key <num>"
+    elif request.method == "GET":
+        data = getDictByJson("data1.json")
+        logger.debug("Get request from IP {}.".format(request.remote_addr))
+        print("Data {}".format(data))
+        return data
 
 if __name__ == "__main__":
     logger = Logger()
