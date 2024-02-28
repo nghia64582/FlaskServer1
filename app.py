9855853372567050b58e6b236ecdb36aeb4e7fa0
nghia64582 <nghia64582@gmail.com>
2024-02-29 01:15:16 +0700

    update socket server

--- app.py
+++ app.py
@@ -4,19 +4,13 @@ import json
 import time
 from datetime import datetime as dt
 from logger import Logger
-import traceback
 import random as rd
 from config import *
 
-print(1)
 app = Flask(__name__)
-print(2)
-logger = Logger()
-print(3)
-logger.debug("Server start at {}".format(dt.now().strftime("%H:%M:%S")))
-print(4)
-app.run(debug = True)
-print(5)
+# logger = Logger()
+# logger.debug("Server start at {}".format(dt.now().strftime("%H:%M:%S")))
+# app.run(debug = True)
 #minor fix 1
 
 def saveDictToJsonFile(data, jsonFile):
@@ -46,3 +40,40 @@ def hello_world():
         return data
     else:
         return "Invalid request"
+
+def startSocket():
+    import socket
+
+    # Define host and port
+    HOST = '103.56.160.36'
+    PORT = 12345
+
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the address and port
+    server_socket.bind((HOST, PORT))
+
+    # Listen for incoming connections
+    server_socket.listen()
+
+    print(f"Server listening on {HOST}:{PORT}...")
+
+    # Accept incoming connections
+    client_socket, client_address = server_socket.accept()
+    print(f"Connected to {client_address}")
+
+    while True:
+        # Receive data from the client
+        data = client_socket.recv(1024).decode('utf-8')
+        if not data:
+            break  # Break the loop if no data is received
+        print(f"Received from client: {data}")
+
+        # Echo the received data back to the client
+        client_socket.sendall(data.encode('utf-8'))
+
+    # Close the connection
+    client_socket.close()
+
+startSocket()
